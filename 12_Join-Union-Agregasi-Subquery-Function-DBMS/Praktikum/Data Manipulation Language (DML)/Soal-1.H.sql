b6ab238b5966c5359f6137a68ec2ff999f77c0cc
Fajar Arasy <fajararasy84@gmail.com>
2023-09-11 00:18:55 +0700

    Tugas Alterra Minggu 4

---
+++ 12_Join-Union-Agregasi-Subquery-Function-DBMS/Praktikum/Data Manipulation Language (DML)/Soal-1.H.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (
+  id, name, status, dob, gender
+)
+VALUES (1, 'Fajar', 200, '2000-10-27', 'M'),
+(2, 'Sidik', 200, '2000-10-28', 'M'),
+(3, 'Syifa', 200, '2000-10-29', 'F'),
+(4, 'Haikal', 200, '2000-10-30', 'M'),
+(5, 'Fitri', 200, '2000-10-31', 'F');
